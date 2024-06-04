4b1926606308dfa9c4ed85e9bfd8a760de6aa760
coldp <coldp@LAPTOP-PA46K82K>
2024-06-05 03:19:51 +0700

    first commit

---
+++ src/data_siswa/ImageIcon.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data_siswa;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author coldp
+ */
+class ImageIcon {
+
+    ImageIcon(String toString) {
+    }
+
+    ImageIcon(Image image) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Object getImage() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
